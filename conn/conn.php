6a1991fe61039ed4ad27423fca9b1628aab18b5f
ElsenHudiyev <114312207+ElsenHudiyev@users.noreply.github.com>
2024-07-02 14:52:10 +0400

    Add files via upload

---
+++ conn/conn.php
@@ -0,0 +1,23 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "1q2w3e";
+$dbname = "online_quiz_db";
+
+//Disable All Cache
+
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache"); // HTTP/1.0
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Failed " . $e->getMessage();
+}
+
+
+?>
